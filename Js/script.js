50359d8a17eea8e99a723bc873211f5ae8b2f91b
SAJNIN123 <62798104+SAJNIN123@users.noreply.github.com>
2020-07-11 00:43:58 +0600

    file

--- Js/script.js
+++ Js/script.js
@@ -3,11 +3,41 @@ $(function () {
         var scrollSize = $(window).scrollTop()
 
         if(scrollSize < 200){
-            $('#backToTop').hide(1000)
+            $('#backToTop').fadeOut(1000)
         } else {
-                $('#backToTop').show(1000)
+                $('#backToTop').fadeIn(1000)
             }
-        })
+    })
+
+    $('#backToTop').on('click',function () {
+        $('html,body').animate({
+            scrollTop:0
+        },2000)
+    })
+
+    //Animate Div
+    $('#animate').on('click',function () {
+        $('#animateAdd').animate({
+            width:'500px',
+            height:'400px',
+            margin:'70px'
+        },'2000')
+        $('#animateAdd').animate({
+           opacity:'0.7',
+           borderRedius:'5px'
+        },'1000')
+    })
+
+    $('#stop').on('click',function () {
+        $('#animateAdd').stop()
+    })
+
+
+    $('p').css({
+        'color':'blue',
+        'font-size':'30px',
+      
+    })
 })  
 
 
